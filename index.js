772b52a63bf48622338bddebd1a1c23498b7ab8d
mohammadasma <ashu7172.137@gmail.com>
2024-10-15 11:43:13 +0530

    sql changes

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 let express=require("express")
 
 let app=express();
+let mysql2=require("mysql2");
+
+
+  let conn=mysql2.createConnection({
+      host:"localhost",
+      user:"root",
+      password:"123456",
+      database:"employe"
+})
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("connected to mysql database")
+    }
+})
 
 app.get("/",(req,res)=>{
 
     res.send("hello")
 })
  let port=3005;
- 
+
 app.listen(port,()=>{
 
     console.log(`port connected:${port}`)
